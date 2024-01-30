ddb54d58839f7c94f250e0f59cf946c4fe92ccb0
venom <abdo30201080101694@gmail.com>
2024-01-30 22:27:29 +0200

    finish left area jsx

--- src/components/GuessGame.jsx
+++ src/components/GuessGame.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import GameArea from "./GameArea";
 
 export default function GuessGame() {
-  return <div className="guess-game"></div>;
+    return <div className="guess-game">
+      <GameArea />
+  </div>;
 }
