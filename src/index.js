2f9904a97791659157d7e44677a9e4f6574259b5
venom <abdo30201080101694@gmail.com>
2024-02-02 23:10:59 +0200

    add game options

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
+//game-name
 export const gameName = `Guess The Word`;
-
 document.title = gameName;
+
+//game-options
+export const numberOfTries = 6;
+export const numberOfLettersPerTry = 6;
