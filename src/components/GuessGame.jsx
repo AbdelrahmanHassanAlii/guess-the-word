91d94aeef66b72f20a119832172af240b948b8c9
venom <abdo30201080101694@gmail.com>
2024-02-02 20:18:12 +0200

    add right area to full area

--- src/components/GuessGame.jsx
+++ src/components/GuessGame.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import GameArea from "./GameArea";
+import GuideArea from "./GuideArea";
 
 export default function GuessGame() {
   return (
     <div className="guess-game">
+      <GuideArea />
       <GameArea />
     </div>
   );
