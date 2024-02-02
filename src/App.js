f04467140794a5357393abccbe59497a33c28532
venom <abdo30201080101694@gmail.com>
2024-02-02 22:40:46 +0200

    Add footer jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
+import Footer from "./components/Footer";
 import GuessGame from "./components/GuessGame";
 import Header from "./components/Header";
-
 
 function App() {
   return (
     <div className="App">
       <Header />
       <GuessGame />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { gameName } from "../index";
+
+export default function Footer() {
+    return <div>{`${gameName} made with love by Abdelrahman`}</div>;
+}
