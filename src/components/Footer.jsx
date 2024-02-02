4d6377205e114a7b4031199597cb69ccf149d28f
venom <abdo30201080101694@gmail.com>
2024-02-02 22:52:28 +0200

    finish footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { gameName } from "../index";
+import "../css/footer.css";
 
 export default function Footer() {
-    return <div>{`${gameName} made with love by Abdelrahman`}</div>;
+  return (
+    <div className="footer">
+      {`${gameName} made with 💚 by`}{" "}
+      <a href="#" className="proto">
+        Abdelrahman
+      </a>
+    </div>
+  );
 }
