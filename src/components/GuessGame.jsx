ca6eea37ba0f3bbd24070b407af8bd712f49bcb8
venom <abdo30201080101694@gmail.com>
2024-02-02 20:50:29 +0200

    handle right and left side in screens

--- src/components/GuessGame.jsx
+++ src/components/GuessGame.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GameArea from "./GameArea";
 import GuideArea from "./GuideArea";
+import '../css/guessGame.css'
 
 export default function GuessGame() {
   return (
